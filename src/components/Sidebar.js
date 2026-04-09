309ecbcdef09dd2ac74ca2b1d37707e51bd146d5
Lis <elis_jasper@estudante.sesisenai.org.br>
2026-04-09 14:32:22 -0300

    fc

---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+export default function Sidebar(){
+    return (
+        <aside className="w-64 bg-white border-r border-gray-200 flex flex-col hidden md:flex">
+            
+            <div className="h-16 flex items-cente px-6 border-b border-gray-100">
+                <span className="text-xl font-bold text-green-800">InvestApp</span>
+            </div>
+        </aside>
+    );
+}
